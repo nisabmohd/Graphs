38722eef173feee5faf64c80f8c93948608682b2
nisabmohd <nisabmohd@gmail.com>
2022-11-12 15:44:20 +0530

    Detecting cycle in undirected graph using bfs and dfs added

---
+++ src/GraphAlgos/DetectCycleUG_BFS.java
@@ -0,0 +1,53 @@
+package GraphAlgos;
+
+import java.util.*;
+
+public class DetectCycleUG_BFS {
+
+    public static void main(String[] args) {
+        Map<Integer, ArrayList<Integer>> map = GraphAlgos.Representation.getDirectedGraph();
+        int n = map.size();
+        Set<Integer> vis = new HashSet<>();
+        boolean flag = true;
+        for (int i = 0; i < n; i++) {
+            if (!vis.contains(i)) {
+                if (hasCycle(map, i, vis)) {
+                    flag = false;
+                }
+            }
+        }
+        if (!flag) {
+            System.out.println("Has Cycle");
+        } else {
+            System.out.println("No cycle detected");
+        }
+    }
+
+    private static boolean hasCycle(Map<Integer, ArrayList<Integer>> map, int s, Set<Integer> vis) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(s, -1));
+        while (!q.isEmpty()) {
+            Pair node = q.poll();
+            for (int item : map.get(node.val)) {
+                if (!vis.contains(item)) {
+                    vis.add(item);
+                    q.add(new Pair(item, node.val));
+                } else if (item != node.parent) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static class Pair {
+
+        int val, parent;
+
+        public Pair(int val, int parent) {
+            this.val = val;
+            this.parent = parent;
+        }
+
+    }
+}
---
+++ src/GraphAlgos/DetectCycleUG_DFS.java
@@ -0,0 +1,40 @@
+package GraphAlgos;
+
+import java.util.*;
+
+public class DetectCycleUG_DFS {
+
+    public static void main(String[] args) {
+        Map<Integer, ArrayList<Integer>> map = GraphAlgos.Representation.getUndirectedGraph();
+        Set<Integer> vis = new HashSet<>();
+        int n = map.size();
+        boolean flag = true;
+        for (int i = 0; i < n; i++) {
+            if (hasCycle(map, i, n, vis)) {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag) {
+            System.out.println("Contains Cycle");
+        } else {
+            System.out.println("Does not contains cycle");
+        }
+
+    }
+
+    public static boolean hasCycle(Map<Integer, ArrayList<Integer>> map, int source, int parent, Set<Integer> vis) {
+        if (vis.contains(source)) {
+            return true;
+        }
+        vis.add(source);
+        for (int item : map.get(source)) {
+            if (item != parent) {
+                if (hasCycle(map, item, source, vis)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
